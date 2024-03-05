4d2918ffd9a17dd510ee2b83d3a05866a21a9d88
akuppili45 <akuppili45@gmail.com>
2024-03-04 19:26:50 -0800

    changes to popup

--- popup.tsx
+++ popup.tsx
@@ -2,14 +2,15 @@ export {}
 import { useState, useEffect } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [chatInput, setData] = useState("");
+  const [chatResponse, setResponse] = useState("hi there!");
   const [currentTab, setCurrentTab] = useState("");
   const [wineData, setWineData] = useState({});
   const getData = async (url) => {
     try{
       console.log(`getting the data for ${url}`)
       const sanitizedTab = encodeURIComponent(url);
-      const resp = await fetch(`http://127.0.0.1:5000/get-text/${sanitizedTab}`); // this should get the vidId and put the text in cloud storage via vidId/text.txt. this vidId will be the source in invoke function in tubechat-ai
+      const resp = await fetch(`https://tubechat-text.onrender.com/get-text/${sanitizedTab}`); // this should get the vidId and put the text in cloud storage via vidId/text.txt. this vidId will be the source in invoke function in tubechat-ai
       const json = await resp.json();
       // call ai server with vidId
       setWineData(json);
@@ -53,16 +54,25 @@ function IndexPopup() {
         </a>{" "}
         Extension!
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <input onChange={(e) => setData(e.target.value)} value={chatInput} />
       <button onClick={() => {
         // data is chat
         if(wineData.message !== "not valid youtube"){
           console.log("valid");
-          // wineData.message pass to vector store
+          fetch(`https://tubechat-ai.onrender.com/invoke/${wineData.video_id}/${chatInput}`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            setResponse(data.chatResponse);
+          })
+          .catch(error => {
+            console.error("Error sending data:", error);
+          });
         }
         console.log("Hi");
+        
       }}>send</button>
-      {/* <p>{wineData.message}</p> */}
+      <p>{chatResponse}</p>
     </div>
   )
 }
